78735a67bf495df49ff8551fa082e7a0519a08b5
Massimo Ronca <massimo@mikamai.com>
2018-10-14 18:44:02 +0200

    add FPS indicator in FPS multiplier

--- lib/components/fps_multiplier.ex
+++ lib/components/fps_multiplier.ex
@@ -7,6 +7,8 @@ defmodule ColorCycling.Component.FPSMultiplier do
 
   @multipliers [1 / 8, 1 / 4, 1 / 2, 3 / 4, 1, 1.25, 1.5]
 
+  @base_fps 60
+
   # --------------------------------------------------------
   def verify(scene) when is_atom(scene), do: {:ok, scene}
   def verify({scene, _} = data) when is_atom(scene), do: {:ok, data}
@@ -15,22 +17,23 @@ defmodule ColorCycling.Component.FPSMultiplier do
   def init(_data, _opts, _parent) do
     graph =
       Graph.build(font: :roboto, font_size: 24)
-      |> text("FPS mulitplier: 1", id: :fps_multiplier_text, t: {0, 10})
+      |> text("FPS mulitplier: 1 / FPS: #{@base_fps}", id: :fps_multiplier_text, t: {0, 10})
       |> slider({@multipliers, 1}, id: :fps_multiplier_slider, t: {0, 20})
       |> push_graph()
 
-    {:ok, graph}
+    {:ok, %{graph: graph, fps: @base_fps}}
   end
 
-  def filter_event({:value_changed, :fps_multiplier_slider, multiplier}, _pid, graph) do
-    text = ~s(FPS mulitplier: #{multiplier})
+  def filter_event({:value_changed, :fps_multiplier_slider, multiplier}, _pid, state) do
+    fps = trunc(@base_fps * multiplier)
+    text = ~s(FPS mulitplier: #{multiplier} / FPS: #{fps})
 
     graph =
-      graph
+      state.graph
       |> Graph.modify(:fps_multiplier_text, &text(&1, text, []))
       |> push_graph()
 
-    send_event({:fps, 60 * multiplier})
-    {:stop, graph}
+    send_event({:fps, fps})
+    {:stop, %{state | graph: graph, fps: fps}}
   end
 end
